f06a174b6dc85a0ff050f1cff69b939810910e6b
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-06-18 14:49:56 -0700

    update build

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,39 +5,43 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "@babel/eslint-parser",
   },
-  extends: [
-    'plugin:vue/recommended',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/recommended", "eslint:recommended"],
   // add your custom rules here
   rules: {
-    'eol-last': 'error',
-    'space-before-function-paren': 'error',
-    'comma-dangle': ['error', 'never'],
-    'semi': ['error', 'never'],
-    'quotes': ['error', 'single', { 'allowTemplateLiterals': true } ],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case', {
-      'registeredComponentsOnly': true,
-      'ignores': []
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      'singleline': 'never',
-      'multiline': 'never'
-    }],
+    "eol-last": "error",
+    "space-before-function-paren": "error",
+    "comma-dangle": ["error", "never"],
+    semi: ["error", "never"],
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
+    "vue/component-name-in-template-casing": [
+      "error",
+      "kebab-case",
+      {
+        registeredComponentsOnly: true,
+        ignores: [],
+      },
+    ],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        singleline: "never",
+        multiline: "never",
+      },
+    ],
 
-    'indent': ['error', 2],
-    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    indent: ["error", 2],
+    "vue/script-indent": ["error", 2, { baseIndent: 1 }],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   overrides: [
     {
-      'files': ['*.vue'],
-      'rules': {
-        'indent': 'off'
-      }
-    }
-  ]
-}
+      files: ["*.vue"],
+      rules: {
+        indent: "off",
+      },
+    },
+  ],
+};
